1a14952610203720b23c94fe196bfdf52df1aeb9
FiveN1 <tomas.verheyen222@gmail.com>
2025-10-08 16:52:11 +0200

    compile and include update

--- build.bat
+++ build.bat
@@ -10,11 +10,13 @@ echo.
 
 set /p user_input=
 
-IF "%user_input%" == ""         GOTO PrintHelp
-IF "%user_input%" == "help"     GOTO PrintHelp
-IF "%user_input%" == "compile"  GOTO Compile
-IF "%user_input%" == "gen_include"  GOTO GenerateInclude
-IF "%user_input%" == "exit"     EXIT
+IF "%user_input%" == ""             GOTO PrintHelp
+IF "%user_input%" == "help"         GOTO PrintHelp
+IF "%user_input%" == "compile_d"    GOTO CompileDebug
+IF "%user_input%" == "compile"      GOTO CompileDebug
+IF "%user_input%" == "compile_r"    GOTO CompileRelease
+IF "%user_input%" == "include"      GOTO GenerateInclude
+IF "%user_input%" == "exit"         EXIT
 
 ::
 :: PREMAKE COMMANDS
@@ -34,19 +36,60 @@ GOTO Done
 echo.
 echo Enter 'build.bat action' where action is one of the following:
 echo.
-echo   compile           Compiles the %library_name% library.
-echo   gen_include       Copies all headers of the library into the include directory.
+echo   compile_d/compile Compiles the %library_name% library in a Debug confoguration using vs2022 solution.
+echo   compile_r         Compiles the %library_name% library in a Release confoguration using vs2022 solution.
+echo   include           Creates an include directory for use in your projects.
 echo   clean             Removes binaries, includes and solution files.
 echo   exit              Exit from this program.
+echo.
+echo Premake commands:
+echo.
+:: premake commands
+echo   codelite          Generate CodeLite project files
+echo   gmake             Generate GNU makefiles for Linux
+echo   vs2005            Generate Visual Studio 2005 project files
+echo   vs2008            Generate Visual Studio 2008 project files
+echo   vs2010            Generate Visual Studio 2010 project files
+echo   vs2012            Generate Visual Studio 2012 project files
+echo   vs2013            Generate Visual Studio 2013 project files
+echo   vs2015            Generate Visual Studio 2015 project files
+echo   vs2017            Generate Visual Studio 2017 project files
+echo   vs2019            Generate Visual Studio 2019 project files
+echo   vs2022            Generate Visual Studio 2022 project files
+echo   xcode4            Generate Apple Xcode 4 project files
+echo.
+echo NOTE: this batch file will run any premake command.
 echo.
 
 GOTO Done
 
 ::
-:: Compile
+:: Generate include
 ::
 
-:Compile
+:GenerateInclude
+
+set headers_output_directory=include\
+
+echo.
+if not exist "%headers_output_directory%" mkdir %headers_output_directory%
+:: copy nymphaea header files
+:: NOTE: pch.h will also be copied (REMOVE THIS!)
+xcopy "src\**.h" "%headers_output_directory%" /E
+:: copy nymphaea dependencies
+xcopy "dependencies\include\**.*" "%headers_output_directory%" /E
+
+echo.
+echo Fetched all headers to %headers_output_directory%
+echo.
+
+GOTO Done
+
+::
+:: Compile Debug
+::
+
+:CompileDebug
 echo.
 
 premake\premake5.exe vs2022
@@ -63,20 +106,21 @@ echo.
 GOTO Done
 
 ::
-:: Fetch headers
+:: Compile Release
 ::
 
-:GenerateInclude
-
-set headers_output_directory=include\%library_name%
-
+:CompileRelease
 echo.
-if not exist "%headers_output_directory%" mkdir %headers_output_directory%
 
-xcopy "src\**.h" "%headers_output_directory%" /E
+premake\premake5.exe vs2022
 
-echo.
-echo Fetched all headers to %headers_output_directory%
+if not defined DevEnvDir (
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+)
+
+set solutionFile="%library_name%.sln"
+msbuild /t:Build /p:Configuration=Release /p:Platform=x64 %solutionFile%
+
 echo.
 
 GOTO Done
